355eb132314896a0ad63fc78751dc581454d2b69
Rauan Caracciolo <rauancaracciolo@gmail.com>
2025-06-21 09:58:36 -0300

    Training and comparing each model.

--- TitanicCompareModels.py
+++ TitanicCompareModels.py
@@ -10,6 +10,9 @@ from sklearn.neighbors import RadiusNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 titanic_df = pd.read_csv('datasets/titanic_processed_train.csv')
 FEATURES = list(titanic_df.columns[1:]) #Extract the features(Columns name) from the data, except the Survived
@@ -73,6 +76,67 @@ def linearDiscrAna_fn(x_train, y_train):
     model.fit(x_train, y_train)
     return model
 
+def sgd_fn(x_train, y_train):
+    model = SGDClassifier()
+    model.fit(x_train, y_train)
+    return model
+
+def qda_fn(x_train, y_train):
+    model = QuadraticDiscriminantAnalysis()
+    model.fit(x_train, y_train)
+    return model
+
+def svc_fn(x_train, y_train):
+    model = LinearSVC()
+    model.fit(x_train, y_train)
+    return model
+
+def gnb_fn(x_train, y_train):
+    model = GaussianNB()
+    model.fit(x_train, y_train)
+    return model
+
+def dtree_fn(x_train, y_train):
+    model = DecisionTreeClassifier()
+    model.fit(x_train, y_train)
+    return model
+
+
+
 result_dict['survived ~ logistic'] = build_model(logistic_fn, 'Survived', FEATURES, titanic_df)
 result_dict['survived ~ linear discriminant analysis'] = build_model(linearDiscrAna_fn, 'Survived', FEATURES, titanic_df)
-compare_results()
+result_dict['survived ~ logistic regression'] = build_model(logistic_fn, 'Survived', FEATURES, titanic_df)
+result_dict['survived ~ linear discriminant'] = build_model(linearDiscrAna_fn, 'Survived', FEATURES, titanic_df)
+result_dict['survived ~ sgd classifier'] = build_model(sgd_fn, 'Survived', FEATURES, titanic_df)
+result_dict['survived ~ quadratic discriminant'] = build_model(qda_fn, 'Survived', FEATURES, titanic_df)
+result_dict['survived ~ linear svc'] = build_model(svc_fn, 'Survived', FEATURES, titanic_df)
+result_dict['survived ~ gaussian nb'] = build_model(gnb_fn, 'Survived', FEATURES, titanic_df)
+result_dict['survived ~ decision tree'] = build_model(dtree_fn, 'Survived', FEATURES, titanic_df)
+
+models = []
+accuracies = []
+precisions = []
+recalls = []
+
+for key in result_dict:
+    models.append(key)
+    accuracies.append(result_dict[key]['test']['accuracy'])
+    precisions.append(result_dict[key]['test']['precision'])
+    recalls.append(result_dict[key]['test']['recall'])
+
+# Plotar
+x = np.arange(len(models))
+width = 0.25
+
+fig, ax = plt.subplots(figsize=(12, 6))
+ax.bar(x - width, accuracies, width, label='Accuracy')
+ax.bar(x, precisions, width, label='Precision')
+ax.bar(x + width, recalls, width, label='Recall')
+
+ax.set_ylabel('Score')
+ax.set_title('Comparison of Classifiers on Titanic Dataset')
+ax.set_xticks(x)
+ax.set_xticklabels(models, rotation=45, ha='right')
+ax.legend()
+plt.tight_layout()
+plt.show()
